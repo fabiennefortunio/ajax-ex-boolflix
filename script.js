ff3e6d3050efaf5024d2c59a7a98b136c2b5d433
Fabienne <fortunio.fabienne@gmail.com>
2019-12-16 17:29:24 +0100

    append

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    var api_url = 'https://api.themoviedb.org/3';
+    // salvo la mia Api
+
+    //Aggiungo il Handlebars tempate che mi serve per raccogliere i dati
     var template = Handlebars.compile("template_film");
-    //svuoto l'imput
-    $('.cerca_film').val('');
 
-    $.ajax({
-        'url': api_url + '/search/movie',
-        'data': {
-            'api_key':'5f9e408b31b5eab1cd8a1f5bf65e5ae3'
-            'query': 'Fight Club',
-            'language': 'en-EN'
-        },
-        'method':'GET',
-        'success': function(data_response) {
-            console.log(data_response);
-            var film = data_response.results;
-            for (var i = 0; i < film.lenght; i++) {
-                var current_film = film[i];
-                var title = film_corrente.title;
-                console.log(title);
-                var original_title = film.original_title
-                var language = film.language
-                var vote = vote.film
-            }
-        },
-        'error' : function() {
-            alert('error');
-        }
+
+    $('.search_movie').keypress(function() {
+        if(keycode == 13);
+});
+
+    //AJAX SECTION
+
+    var results = $('#template_film').append(function(){
+
     });
-    // ha detto Sofia fare append?
+
+    $('#search_button').click(function(){
+        api_search();
+    });
+    var api_search = function() {
+        var search_movie = $('.search_movie').val();
+        var api_url = 'https://api.themoviedb.org/3';
+        $.ajax({
+            'url': api_url + '/search/movie',
+            'data': {
+                'api_key': '5f9e408b31b5eab1cd8a1f5bf65e5ae3',
+                'query': search_movie,
+                'language': 'en-EN'
+            },
+            'method': 'GET',
+            'success': function(data_response) {
+                console.log(data_response);
+                var film = data_response.results;
+                for (var i = 0; i < film.lenght; i++) {
+                    var current_film = film[i];
+                    var title = film_corrente.title;
+                    console.log(title);
+                    var original_title = film.original_title
+                    var language = film.language
+                    var vote = vote.film
+                }
+            },
+            'error': function() {
+                alert('error');
+            }
+        });
+
+    }
+
+
+// come incollare sul html tramite handlebars i results
+//keypress per fare una ricerca con Enter 
 
 });
